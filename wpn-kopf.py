bea8cc58701f3a074d66cd6a415090f2a5064442
Rosa Maggi <rosa.maggi@epfl.ch>
2024-10-31 09:14:17 +0100

    [fix] call php files for manage_plugin and ensure-wordpress-and-theme without parameters

--- wpn-kopf.py
+++ wpn-kopf.py
@@ -155,7 +155,7 @@ class WordPressSiteOperator:
   def install_wordpress_via_php(self, path, title, tagline):
       logging.info(f" ↳ [install_wordpress_via_php] Configuring (ensure-wordpress-and-theme.php) with {self.name=}, {path=}, {title=}, {tagline=}")
       # https://stackoverflow.com/a/89243
-      result = subprocess.run([Config.php, Config.wp_php_ensure,
+      result = subprocess.run([Config.php, "ensure-wordpress-and-theme.php",
                                f"--name={self.name}", f"--path={path}",
                                f"--wp-dir={Config.wp_dir}",
                                f"--wp-host={Config.wp_host}",
@@ -173,7 +173,7 @@ class WordPressSiteOperator:
   def manage_plugins_php(self, plugins):
       logging.info(f" ↳ [manage_plugins_php] Configuring (manage-plugins.php) with {self.name=} and {plugins=}")
       # https://stackoverflow.com/a/89243
-      result = subprocess.run([Config.php, Config.wp_php_ensure,
+      result = subprocess.run([Config.php, "manage-plugins.php",
                                f"--name={self.name}",
                                f"--wp-dir={Config.wp_dir}",
                                f"--wp-host={Config.wp_host}",
