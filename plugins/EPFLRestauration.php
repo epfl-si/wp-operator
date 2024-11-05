37ad04223c595f306fe4bbbfc009dd11b1ec1986
Rosa Maggi <rosa.maggi@epfl.ch>
2024-11-05 09:09:55 +0100

    [fix] Create function to get secrets

--- plugins/EPFLRestauration.php
+++ plugins/EPFLRestauration.php
@@ -5,5 +5,15 @@ class EPFLRestauration extends Plugin
 	protected $pluginPath = "epfl-restauration/epfl-restauration.php";
 	private $epfl_restauration_api_username = 'epfl.getmenu@nutrimenu.ch';
 	private $epfl_restauration_api_url = 'https://nutrimenu.ch/nmapi/getMenu';
-	private $epfl_restauration_api_password = 'XXX';
+
+	private function epfl_restauration_api_password() {
+		return $this->getSecretValue('restauration_api_password');
+	}
+
+	public function updateOptions()
+	{
+		update_option( 'wpforms_challenge', $this->epfl_restauration_api_username);
+		update_option( 'wpforms_settings', $this->epfl_restauration_api_url);
+		update_option( 'wpforms_license', $this->epfl_restauration_api_password());
+	}
 }
