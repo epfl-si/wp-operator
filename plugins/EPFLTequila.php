0783bb83052de045cea0de3f91047d813c2a8d2b
Rosa Maggi <rosa.maggi@epfl.ch>
2024-11-05 18:27:02 +0100

    [fix] plugins options

--- plugins/EPFLTequila.php
+++ plugins/EPFLTequila.php
@@ -3,4 +3,11 @@
 class EPFLTequila extends Plugin
 {
 	protected $pluginPath = "tequila/EPFL-Tequila.php";
+	private $plugin_epfl_tequila_has_dual_auth = 0;
+
+	public function updateOptions()
+	{
+		update_option( 'plugin:epfl_tequila:has_dual_auth', $this->plugin_epfl_tequila_has_dual_auth);
+		update_option( 'plugin:epfl_accred:unit_id', $this->plugin_epfl_accred_unit_id);
+	}
 }
