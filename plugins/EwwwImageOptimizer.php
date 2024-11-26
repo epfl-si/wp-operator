067871a6dbfb96f68a0e624b389e488c0995f233
Rosa Maggi <rosa.maggi@epfl.ch>
2024-11-26 16:57:56 +0100

    [fix] private variables --> protected variables

--- plugins/EwwwImageOptimizer.php
+++ plugins/EwwwImageOptimizer.php
@@ -3,23 +3,23 @@
 class EwwwImageOptimizerPlugin extends Plugin
 {
 	protected $pluginPath = "ewww-image-optimizer/ewww-image-optimizer.php";
-	private $exactdn_all_the_things = '';
-	private $exactdn_lossy = '';
-	private $ewww_image_optimizer_tracking_notice = '1';
-	private $ewww_image_optimizer_enable_help_notice = '1';
-	private $ewww_image_optimizer_cloud_key = '';
-	private $ewww_image_optimizer_jpg_quality = '';
-	private $ewww_image_optimizer_include_media_paths = '1';
-	private $ewww_image_optimizer_aux_paths = '';
-	private $ewww_image_optimizer_exclude_paths = '';
-	private $ewww_image_optimizer_allow_tracking = '';
-	private $ewww_image_optimizer_maxmediawidth = '2048';
-	private $ewww_image_optimizer_maxmediaheight = '2048';
-	private $ewww_image_optimizer_resize_existing = '1';
-	private $ewww_image_optimizer_disable_resizes = '';
-	private $ewww_image_optimizer_disable_resizes_opt = '';
-	private $ewww_image_optimizer_jpg_background = '';
-	private $ewww_image_optimizer_webp_paths = '';
-	private $ewww_image_optimizer_dismiss_media_notice = '1';
-	private $ewww_image_optimizer_debug = '';
+	protected $exactdn_all_the_things = '';
+	protected $exactdn_lossy = '';
+	protected $ewww_image_optimizer_tracking_notice = '1';
+	protected $ewww_image_optimizer_enable_help_notice = '1';
+	protected $ewww_image_optimizer_cloud_key = '';
+	protected $ewww_image_optimizer_jpg_quality = '';
+	protected $ewww_image_optimizer_include_media_paths = '1';
+	protected $ewww_image_optimizer_aux_paths = '';
+	protected $ewww_image_optimizer_exclude_paths = '';
+	protected $ewww_image_optimizer_allow_tracking = '';
+	protected $ewww_image_optimizer_maxmediawidth = '2048';
+	protected $ewww_image_optimizer_maxmediaheight = '2048';
+	protected $ewww_image_optimizer_resize_existing = '1';
+	protected $ewww_image_optimizer_disable_resizes = '';
+	protected $ewww_image_optimizer_disable_resizes_opt = '';
+	protected $ewww_image_optimizer_jpg_background = '';
+	protected $ewww_image_optimizer_webp_paths = '';
+	protected $ewww_image_optimizer_dismiss_media_notice = '1';
+	protected $ewww_image_optimizer_debug = '';
 }
--- plugins/WPMediaFolder.php
+++ plugins/WPMediaFolder.php
@@ -3,40 +3,40 @@
 class WPMediaFolderPlugin extends Plugin
 {
 	protected $pluginPath = "wp-media-folder/wp-media-folder.php";
-	private $wpmf_use_taxonomy ='1';
-	private $wpmf_gallery_image_size_value ='["thumbnail","medium","large","full"]';
-	private $wpmf_padding_masonry ='5';
-	private $wpmf_padding_portfolio ='10';
-	private $wpmf_usegellery ='0';
-	private $wpmf_useorder ='1';
-	private $wpmf_create_folder ='role';
-	private $wpmf_option_override ='1';
-	private $wpmf_option_duplicate ='0';
-	private $wpmf_active_media ='0';
-	private $wpmf_folder_option2 ='1';
-	private $wpmf_option_searchall ='1';
-	private $wpmf_usegellery_lightbox ='0';
-	private $wpmf_media_rename ='0';
-	private $wpmf_patern_rename ='{sitename} - {foldername} - #';
-	private $wpmf_rename_number ='0';
-	private $wpmf_option_media_remove ='0';
-	private $wpmf_default_dimension ='["400x300","640x480","800x600","1024x768","1600x1200"]';
-	private $wpmf_selected_dimension ='["400x300","640x480","800x600","1024x768","1600x1200"]';
-	private $wpmf_weight_default ='[["0-61440","kB"],["61440-122880","kB"],["122880-184320","kB"],["184320-245760","kB"],["245760-307200","kB"]]';
-	private $wpmf_weight_selected ='[["0-61440","kB"],["61440-122880","kB"],["122880-184320","kB"],["184320-245760","kB"],["245760-307200","kB"]]';
-	private $wpmf_color_singlefile ='{"bgdownloadlink":"#444444","hvdownloadlink":"#888888","fontdownloadlink":"#ffffff","hoverfontcolor":"#ffffff"}';
-	private $wpmf_option_singlefile ='0';
-	private $wpmf_option_sync_media ='0';
-	private $wpmf_option_sync_media_external ='0';
-	private $wpmf_list_sync_media ="array ()";
-	private $wpmf_time_sync ='60';
-	private $wpmf_lastRun_sync ='1540467937';
-	private $wpmf_slider_animation ='slide';
-	private $wpmf_option_mediafolder ='0';
-	private $wpmf_option_countfiles ='1';
-	private $wpmf_option_lightboximage ='0';
-	private $wpmf_option_hoverimg ='1';
-	private $wpmf_options_format_title = array (
+	protected $wpmf_use_taxonomy ='1';
+	protected $wpmf_gallery_image_size_value ='["thumbnail","medium","large","full"]';
+	protected $wpmf_padding_masonry ='5';
+	protected $wpmf_padding_portfolio ='10';
+	protected $wpmf_usegellery ='0';
+	protected $wpmf_useorder ='1';
+	protected $wpmf_create_folder ='role';
+	protected $wpmf_option_override ='1';
+	protected $wpmf_option_duplicate ='0';
+	protected $wpmf_active_media ='0';
+	protected $wpmf_folder_option2 ='1';
+	protected $wpmf_option_searchall ='1';
+	protected $wpmf_usegellery_lightbox ='0';
+	protected $wpmf_media_rename ='0';
+	protected $wpmf_patern_rename ='{sitename} - {foldername} - #';
+	protected $wpmf_rename_number ='0';
+	protected $wpmf_option_media_remove ='0';
+	protected $wpmf_default_dimension ='["400x300","640x480","800x600","1024x768","1600x1200"]';
+	protected $wpmf_selected_dimension ='["400x300","640x480","800x600","1024x768","1600x1200"]';
+	protected $wpmf_weight_default ='[["0-61440","kB"],["61440-122880","kB"],["122880-184320","kB"],["184320-245760","kB"],["245760-307200","kB"]]';
+	protected $wpmf_weight_selected ='[["0-61440","kB"],["61440-122880","kB"],["122880-184320","kB"],["184320-245760","kB"],["245760-307200","kB"]]';
+	protected $wpmf_color_singlefile ='{"bgdownloadlink":"#444444","hvdownloadlink":"#888888","fontdownloadlink":"#ffffff","hoverfontcolor":"#ffffff"}';
+	protected $wpmf_option_singlefile ='0';
+	protected $wpmf_option_sync_media ='0';
+	protected $wpmf_option_sync_media_external ='0';
+	protected $wpmf_list_sync_media ="array ()";
+	protected $wpmf_time_sync ='60';
+	protected $wpmf_lastRun_sync ='1540467937';
+	protected $wpmf_slider_animation ='slide';
+	protected $wpmf_option_mediafolder ='0';
+	protected $wpmf_option_countfiles ='1';
+	protected $wpmf_option_lightboximage ='0';
+	protected $wpmf_option_hoverimg ='1';
+	protected $wpmf_options_format_title = array (
 		'hyphen' => '1',
 		'period' => '0',
 		'plus' => '0',
@@ -53,18 +53,18 @@ class WPMediaFolderPlugin extends Plugin
 		'caption' => '0',
 		'capita' => 'cap_all',
 	);
-	private $wpmf_image_watermark_apply = array (
+	protected $wpmf_image_watermark_apply = array (
 		'all_size' => '1',
 		'thumbnail' => '0',
 		'medium' => '0',
 		'large' => '0',
 		'full' => '0',
 	);
-	private $wpmf_option_image_watermark ='0';
-	private $wpmf_watermark_position ='top_left';
-	private $wpmf_watermark_image ='';
-	private $wpmf_watermark_image_id ='0';
-	private $wpmf_gallery_settings ="array (
+	protected $wpmf_option_image_watermark ='0';
+	protected $wpmf_watermark_position ='top_left';
+	protected $wpmf_watermark_image ='';
+	protected $wpmf_watermark_image_id ='0';
+	protected $wpmf_gallery_settings ="array (
 				hyphen => 1,
 				period => 0,
 				plus => 0,
@@ -81,7 +81,7 @@ class WPMediaFolderPlugin extends Plugin
 				caption => 0,
 				capita => cap_all,
 			)";
-	private $wpmf_settings ="array (
+	protected $wpmf_settings = "array (
 				hide_remote_video => 0,
 				gallery_settings =>
 					array (
@@ -145,6 +145,6 @@ class WPMediaFolderPlugin extends Plugin
 				watermark_image_scaling => 100,
 				format_mediatitle => 1,
 			)";
-	private $_wpmf_import_order_notice_flag ='yes';
-	private $can_compress_scripts ='0';
+	protected $_wpmf_import_order_notice_flag ='yes';
+	protected $can_compress_scripts ='0';
 }
