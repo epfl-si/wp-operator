d23e4fd6a0a3b5486a1aee2edc23236ac38d81fb
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-12-03 14:59:47 +0100

    [rewrite] with `kubernetes_asyncio`

--- olm/wpn-olm-controller.py
+++ olm/wpn-olm-controller.py
@@ -1,77 +1,44 @@
 import kopf
 import kopf.cli
-from kubernetes import client, config
-from kubernetes.dynamic import DynamicClient
-from kubernetes.client.exceptions import ApiException
+from kubernetes_asyncio import client, config
+from kubernetes_asyncio.client import ApiClient
+from kubernetes_asyncio.dynamic import DynamicClient
+from kubernetes_asyncio.client.exceptions import ApiException
 import logging
 import os
 import sys
 import yaml
 
 
-
-class classproperty:
-    def __init__(self, func):
-        self.fget = func
-    def __get__(self, instance, owner):
-        return self.fget(owner)
-
-
-class KubernetesAPI:
-  __singleton = None
-
-  @classmethod
-  def __get(cls):
-      if cls.__singleton is None:
-          cls.__singleton = cls()
-
-      return cls.__singleton
-
-  def __init__(self):
-      config.load_config()
-
-      self._custom = client.CustomObjectsApi()
-      self._core = client.CoreV1Api()
-      self._extensions = client.ApiextensionsV1Api()
-      self._dynamic = DynamicClient(client.ApiClient())
-      self._networking = client.NetworkingV1Api()
-
-  @classproperty
-  def extensions(cls):
-    return cls.__get()._extensions
-
-  @classproperty
-  def dynamic(cls):
-    return cls.__get()._dynamic
-
-
 @kopf.on.startup()
-def on_kopf_startup (**kwargs):
-    WordPressCRDOperator.ensure_wp_crd_exists()
+async def on_kopf_startup (**kwargs):
+    await WordPressCRDOperator.ensure_wp_crd_exists()
 
 
 class WordPressCRDOperator:
     @classmethod
-    def ensure_wp_crd_exists(cls):
-      dyn_client = KubernetesAPI.dynamic
-      api_extensions_instance = KubernetesAPI.extensions
+    async def ensure_wp_crd_exists(cls):
       crd_name = "wordpresssites.wordpress.epfl.ch"
 
       try:
-          crd_list = api_extensions_instance.list_custom_resource_definition()
-          exists = any(crd.metadata.name == crd_name for crd in crd_list.items)
+          await config.load_kube_config()
+          async with ApiClient() as api:
+              api_extensions = client.ApiextensionsV1Api(api)
+              crd_list = await api_extensions.list_custom_resource_definition()
+              exists = any(crd.metadata.name == crd_name for crd in crd_list.items)
+              
+              if exists:
+                  logging.info(f"↳ CRD '{crd_name}' already exists, doing nothing...")
+                  return True
 
-          if exists:
-              logging.info(f"↳ CRD '{crd_name}' already exists, doing nothing...")
-              return True
-          else:
               logging.info(f"↳ CRD '{crd_name}' does not exists, creating it...")
-              with open("WordPressSite-crd.yaml") as file:
+              with open("WordPressSite-crd.yaml") as file:  # TODO: blocking
                   crd_file = yaml.safe_load(file)
-              crd_resource = dyn_client.resources.get(api_version='apiextensions.k8s.io/v1', kind='CustomResourceDefinition')
+              dyn_client = await DynamicClient(api)
+              crd_resource = await dyn_client.resources.get(api_version='apiextensions.k8s.io/v1', kind='CustomResourceDefinition')
 
               try:
-                  crd_resource.create(body=crd_file)
+                  await crd_resource.create(body=crd_file)
                   logging.info(f"↳ CRD '{crd_name}' created")
                   return True
               except client.exceptions.ApiException as e:
