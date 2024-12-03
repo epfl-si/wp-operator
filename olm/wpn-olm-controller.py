2bee4c4727b2303f1c921eed06660f1624ad8fa0
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-12-03 15:14:41 +0100

    [feature] Recreate CRD when deleted
    As per
    https://github.com/nolar/kopf/issues/702#issuecomment-2511251889 we
    need to be extra careful in `@on.delete()` callbacks. To wit:
    
    - it appears that the `optional=True` parameter to `on.delete()` does
    exactly what it says on the tin → no event is received 😁
    - a specific filter must be set using `field` and `value`, lest Kopf
    scribble onto each and every CRD in the cluster (although a little
    less scribbling happens when `optional=True`, this behavior cannot
    easily be disabled completely)
    - the `on.delete()` hook must complete before we attempt to recreate
    the object, or deadlock occurs.

--- olm/wpn-olm-controller.py
+++ olm/wpn-olm-controller.py
@@ -1,3 +1,4 @@
+import asyncio
 import kopf
 import kopf.cli
 from kubernetes_asyncio import client, config
@@ -7,46 +8,71 @@ from kubernetes_asyncio.client.exceptions import ApiException
 import logging
 import os
 import sys
+import threading
+import time
 import yaml
 
-
-@kopf.on.startup()
-async def on_kopf_startup (**kwargs):
-    await WordPressCRDOperator.ensure_wp_crd_exists()
-
-
 class WordPressCRDOperator:
+    crd_name = "wordpresssites.wordpress.epfl.ch"
+    _kube_config_loaded = False
+
     @classmethod
-    async def ensure_wp_crd_exists(cls):
-      crd_name = "wordpresssites.wordpress.epfl.ch"
+    def hook (cls):
+        self = cls()
+        @kopf.on.startup()
+        async def on_kopf_startup (**kwargs):
+            await self.ensure_crd_exists()
 
-      try:
-          await config.load_kube_config()
-          async with ApiClient() as api:
-              api_extensions = client.ApiextensionsV1Api(api)
-              crd_list = await api_extensions.list_custom_resource_definition()
-              exists = any(crd.metadata.name == crd_name for crd in crd_list.items)
-              
-              if exists:
-                  logging.info(f"↳ CRD '{crd_name}' already exists, doing nothing...")
-                  return True
+        @kopf.on.delete('customresourcedefinition',
+                        field='metadata.name', value=self.crd_name)
+        async def on_kopf_delete_crd (**kwargs):
+            logging.info("CRD is being deleted!")
 
-              logging.info(f"↳ CRD '{crd_name}' does not exists, creating it...")
-              with open("WordPressSite-crd.yaml") as file:  # TODO: blocking
-                  crd_file = yaml.safe_load(file)
-              dyn_client = await DynamicClient(api)
-              crd_resource = await dyn_client.resources.get(api_version='apiextensions.k8s.io/v1', kind='CustomResourceDefinition')
+            async def recreate_later ():
+                for _ in range(0, 30):
+                    if not await self.exists():
+                        await self.ensure_crd_exists()
+                        return
+                    else:
+                        await asyncio.sleep(1)
+                else:
+                    raise kopf.PermanentError("Zombie CRD won't die!")
 
-              try:
-                  await crd_resource.create(body=crd_file)
-                  logging.info(f"↳ CRD '{crd_name}' created")
-                  return True
-              except client.exceptions.ApiException as e:
-                  logging.error("Error trying to create CRD :", e)
-          print("Operator started and initialized")
-      except ApiException as e:
-          logging.error(f"Error verifying CRD file: {e}")
-      return False
+            asyncio.create_task(recreate_later())
+
+    async def _load_kube_config (self):
+        if not self._kube_config_loaded:
+            await config.load_kube_config()
+            self._kube_config_loaded = True
+
+    async def exists (self):
+        await self._load_kube_config()
+        async with ApiClient() as api:
+            api_extensions = client.ApiextensionsV1Api(api)
+            crd_list = await api_extensions.list_custom_resource_definition()
+            return any(crd.metadata.name == self.crd_name for crd in crd_list.items)
+
+    async def ensure_crd_exists(self):
+        await self._load_kube_config()
+        if await self.exists():
+            logging.info(f"↳ CRD '{self.crd_name}' already exists, doing nothing...")
+            return True
+
+        logging.info(f"↳ CRD '{self.crd_name}' does not exists, creating it...")
+        with open("WordPressSite-crd.yaml") as file:  # TODO: blocking
+            crd_file = yaml.safe_load(file)
+        async with ApiClient() as api:
+            dyn_client = await DynamicClient(api)
+            crd_resource = await dyn_client.resources.get(api_version='apiextensions.k8s.io/v1', kind='CustomResourceDefinition')
+          
+            try:
+                await crd_resource.create(body=crd_file)
+                logging.info(f"↳ CRD '{self.crd_name}' created")
+                return True
+            except client.exceptions.ApiException as e:
+                logging.error("Error trying to create CRD :", e)
+                raise e
 
 if __name__ == '__main__':
+    WordPressCRDOperator.hook()
     sys.exit(kopf.cli.main())
