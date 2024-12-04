af659cdbd275a7fca9244434b6068bdfa51d122c
Rosa Maggi <rosa.maggi@epfl.ch>
2024-12-04 16:44:48 +0100

    [fix] comment
    Get plugin secrets from kubectl and save them decoded in the secretFiles directory for development purposes

--- dev/secrets.sh
+++ dev/secrets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir "secretFiles"
 
+# Get plugin secrets from kubectl and save them decoded in the secretFiles directory for development purposes
 jsonSecret=$(kubectl get secrets -n wordpress-test wp-plugin-secrets -o json | jq -r '.data')
 for key in $(echo "$jsonSecret" | jq -r 'keys[]'); do
   # Get the value for the current key
