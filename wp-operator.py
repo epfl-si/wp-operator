56c103d4aa0ea5faa731453d21783d52d2849bea
Nicolas Borboën <ponsfrilus@gmail.com>
2025-01-30 17:14:13 +0100

    [feature] Operator's version
    Isn't `tippelskirchi` a nice codename?

--- wp-operator.py
+++ wp-operator.py
@@ -823,6 +823,7 @@ class NamespaceFromEnv:
     @classmethod
     def setup (cls):
         namespace = cls.get()
+        logging.info(f'WP-Operator v0.0.1 | codename: tippelskirchi')
         logging.info(f'Running in namespace {namespace}')
         os.environ['KUBERNETES_NAMESPACE'] = namespace
         try:
