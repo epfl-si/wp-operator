e7a54560009b3533763b7a0d6f5420b947eb040d
Rosa Maggi <rosa.maggi@epfl.ch>
2025-02-06 11:47:20 +0100

    [fix] for tequila plugin set the allowed hosts

--- plugins/EPFLTequila.php
+++ plugins/EPFLTequila.php
@@ -4,9 +4,11 @@ class EPFLTequilaPlugin extends Plugin
 {
   protected $pluginPath = "tequila/EPFL-Tequila.php";
   private $plugin_epfl_tequila_has_dual_auth = 0;
+  private $plugin_epfl_tequila_allowed_request_hosts = '10.0.0.0/8';
 
   public function updateOptions()
   {
     update_option( 'plugin:epfl_tequila:has_dual_auth', $this->plugin_epfl_tequila_has_dual_auth);
+	update_option( 'plugin:epfl:tequila_allowed_request_hosts', $this->plugin_epfl_tequila_allowed_request_hosts);
   }
 }
