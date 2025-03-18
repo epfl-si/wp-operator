c5b299cf55c1963f40ab47583b3bd526fe8351b2
duratti <lindo.duratti@epfl.ch>
2025-03-18 10:17:30 +0100

    Bump to 1.2.0 : tippelskirchi

--- wp_operator.py
+++ wp_operator.py
@@ -978,7 +978,7 @@ class NamespaceFromEnv:
     @classmethod
     def setup (cls):
         namespace = cls.get()
-        logging.info(f'WP-Operator v1.1.4 | codename: reticulata')
+        logging.info(f'WP-Operator v1.2.0 | codename: tippelskirchi')
         logging.info(f'Running in namespace {namespace}')
         os.environ['KUBERNETES_NAMESPACE'] = namespace
         try:
