aec821b9494b9f88def7d816524eb036528de4d3
Rosa Maggi <rosa.maggi@epfl.ch>
2025-03-20 14:32:20 +0100

    [version] v1.3.0 | Negaprion

--- wp_operator.py
+++ wp_operator.py
@@ -931,7 +931,7 @@ class NamespaceFromEnv:
     @classmethod
     def setup (cls):
         namespace = cls.get()
-        logging.info(f'WP-Operator v1.2.0 | codename: triaenodon')
+        logging.info(f'WP-Operator v1.3.0 | codename: Negaprion')
         logging.info(f'Running in namespace {namespace}')
         os.environ['KUBERNETES_NAMESPACE'] = namespace
         try:
