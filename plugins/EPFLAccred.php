fc1b0741f1bb5ed2b03cd5f08bcac2fc3ee3efd1
Rosa Maggi <rosa.maggi@epfl.ch>
2025-04-11 12:07:03 +0200

    [fix] Set the `plugin:epfl_accred:subscriber_group` option to `*`

--- plugins/EPFLAccred.php
+++ plugins/EPFLAccred.php
@@ -4,6 +4,7 @@ class EPFLAccredPlugin extends Plugin
 {
   protected $pluginPath = "accred/EPFL-Accred.php";
   private $plugin_epfl_accred_administrator_group = "WP-SuperAdmin";
+  private $plugin_epfl_accred_subscriber_group = "*";
   private $plugin_epfl_accred_unit_id;
 
   function __construct($unit_id) {
@@ -13,6 +14,7 @@ class EPFLAccredPlugin extends Plugin
   public function updateOptions()
   {
     update_option( 'plugin:epfl_accred:administrator_group', $this->plugin_epfl_accred_administrator_group);
+    update_option( 'plugin:epfl_accred:subscriber_group', $this->plugin_epfl_accred_subscriber_group);
     update_option( 'plugin:epfl_accred:unit_id', $this->plugin_epfl_accred_unit_id);
   }
 }
