b4b1521e82a3c001a2b9b9f097f6c1a49bb6b322
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-06-18 18:08:16 +0200

    [oh,] chihuahua!

--- wp_operator.py
+++ wp_operator.py
@@ -911,7 +911,7 @@ class NamespaceFromEnv:
     @classmethod
     def setup (cls):
         namespace = cls.get()
-        logging.info(f'WP-Operator v2.1.0 | codename: Samoyed')
+        logging.info(f'WP-Operator v2.1.0 | codename: Chihuahua')
         logging.info(f'Running in namespace {namespace}')
         os.environ['KUBERNETES_NAMESPACE'] = namespace
         try:
